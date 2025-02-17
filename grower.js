077bdda763f7e2bdebf42f8a9bf962a5f3f22b51
LukeOdeski <lukeodeski@gmail.com>
2025-02-17 17:25:05 -0600

    Update grower.js

--- grower.js
+++ grower.js
@@ -1,9 +1,19 @@
 document.getElementById("menu1").addEventListener("click", menuErector);
+document.getElementById("arrow").addEventListener("click", nakedGrandma);
 function menuErector()
 {
     var menu = document.getElementById("menu1");
     var list = document.getElementById("list1");
     
-    menu.classList.toggle("extended");
-    list.classList.toggle("visible");
+    menu.classList.add("extended");
+    list.classList.add("visible");
+}
+
+function nakedGrandma()
+{
+    var menu = document.getElementById("menu1");
+    var list = document.getElementById("list1");
+    
+    menu.classList.remove("extended");
+    list.classList.remove("visible");
 }
