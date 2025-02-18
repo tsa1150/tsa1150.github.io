e433b5349bc5dba4bc8ea9136dea6402565ff13a
LukeOdeski <lukeodeski@gmail.com>
2025-02-17 19:44:23 -0600

    Update grower.js

--- grower.js
+++ grower.js
@@ -4,9 +4,13 @@ function menuErector()
 {
     var menu = document.getElementById("menu1");
     var list = document.getElementById("list1");
+    var image = document.getElementById("arrow");
     
     menu.classList.add("extended");
+    menu.classList.remove('retract');
     list.classList.add("visible");
+    list.classList.remove('hide');
+    image.classList.toggle('show');
 }
 
 function nakedGrandma(e)
@@ -14,7 +18,11 @@ function nakedGrandma(e)
     e.stopPropagation();
     var menu = document.getElementById("menu1");
     var list = document.getElementById("list1");
+    var image = document.getElementById("arrow");
     
-    menu.classList.remove("extended");
+    menu.classList.remove('extended');
+    menu.classList.add("retract");
     list.classList.remove("visible");
+    list.classList.add('hide');
+    image.classList.toggle('show');
 }
