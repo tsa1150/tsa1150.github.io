37091f4f45ee0a3c3f157bd512e86dfb8ce7cbe9
LukeOdeski <lukeodeski@gmail.com>
2025-02-18 00:48:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function getElement(id)
 {
     return document.getElementById(id);
 }
-
+//San Francisco from the Marin Headlands on August 22, 2022
+//Noah Friedlander - Own work
+//This file (The image) is licensed under the Creative Commons Attribution-Share Alike 4.0 International license.
 function dropdownFunction(e)
 {
     e.stopPropagation();
@@ -38,3 +40,20 @@ function dropdownFunction(e)
         text.classList.remove("retracted");
     }
 }
+
+/*
+        <div class="introGrid">
+            <div class="introCard">
+                <h3>Who Are We?</h3>
+                <p>Embodying the bustling and gaudy energy in SF, The Lotus Lab, offers a variety of sophisticated and artfully curated vegetarian dishes, services, and dining experiences. The Asian and American fusion of our cuisine brings a familiar yet interesting take on the city culture and brings comfort food to residents of the bay to a new level while maintaining a vegetarian lifestyle.</p>
+            </div>
+            <div class="introCard">
+                <h3>Our Accomplishments</h3>
+                <p>Best known for its win of the Michelin Star 2 years in a row, we at The Lotus Lab count the most pride in our ability to deliver farm-to-table in an empathic city as well as clean food processes to ensure customer satisfaction and safety.</p>
+            </div>
+            <div class="introCard">
+                <h3>Where Are We?</h3>
+                <p> Home to nearly 800,000, the Golden City has been built with a fusion of American values and Asian style as well as a mass of versatile elements.  Within the heart of downtown San Francisco lies a perfect balance of diversity and inclusivity that “The City” has flourished and thrived under. Tourists and natives alike would expect nothing less when it comes to the culinary scene and arts. </p>
+            </div>
+        </div>
+*/
